1d50800805077bcb4697cccc89c6f104492bf98b
Richard <richard.turza@gmail.com>
2016-12-15 23:53:01 +0000

    just IP

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 "use strict";
 
 var express = require("express");
-var pe_Port = process.env.PORT ;
+var pe_Port = process.env.PORT || 8080;
 
 express()
+.set('port', pe_Port)
 .get("/", (req, res) => {
     
     let response = {"ipadress" : req.connection.remoteAddress};
     
+    res.header("Content-Type", "application/json; charset=utf-8");
     res.end(JSON.stringify(response));
 })
-.listen(pe_Port || 8080, () => {
+.listen(pe_Port, () => {
    console.log("whoAmI running on port ", pe_Port) 
 });

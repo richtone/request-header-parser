151689061cbb96b51b02d34ba18f6ebd69bab1fc
Richard <richard.turza@gmail.com>
2016-12-18 00:39:12 +0000

    finish

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 "use strict";
 
 var express = require("express");
+var useragent = require("useragent.js");
+
 var pe_Port = process.env.PORT || 8080;
 
 express()
 .set('port', pe_Port)
 .get("/", (req, res) => {
     
-    let response = {"ipadress" : req.header('x-forwarded-for')};
+    let ua = useragent.analyze(JSON.stringify(req.header("user-agent")));
+    
+    let response = {"ipaddress" :   req.header('x-forwarded-for'),
+                    "language"  :   req.acceptsLanguages()[0],
+                    "software"  :   ua.os.full
+    };
     
     res.header("Content-Type", "application/json; charset=utf-8");
     res.end(JSON.stringify(response));
